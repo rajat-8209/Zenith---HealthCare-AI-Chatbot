5865b5a2d6ce368ed0fea8c78a78360ab9d8e744
Rajat Gupta <rajatgupta3380@gmail.com>
2025-11-30 23:17:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rajat Gupta",
+    author_email="rajatgupta3380@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
